9ded3c7329a15a1ab9df338e32ba7c11bfa919b1
luc99hen <1813927768@qq.com>
2021-12-10 14:30:10 +0800

    Test: improve frontend workload test

--- frontend/src/components/WorkloadManagement/Workload.test.js
+++ frontend/src/components/WorkloadManagement/Workload.test.js
@@ -1,35 +1,44 @@
 import React from "react";
 import Workload from "./WorkloadTemplate";
-import { render } from "@testing-library/react";
-import userEvent from '@testing-library/user-event'
+import { render, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import "../../utils/jest.mock";
 
 describe("Workload Test", () => {
+  const testTitle = "WorkloadTest";
 
-    const testTitle = "WorkloadTest"
+  it("should render normally with empty table data", () => {
+    const mockAsyncFn = jest.fn(async () => {
+      return Promise.resolve([]);
+    });
 
-    it("should render normally with empty table data", () => {
-        const mockAsyncFn = jest.fn(async () => {
-            return Promise.resolve([])
-        })
+    const { getByText } = render(
+      <Workload title={testTitle} table={{ fetchData: mockAsyncFn }}></Workload>
+    );
 
-        const { getByText } = render(<Workload title={testTitle} table={{ fetchData: mockAsyncFn }} ></ Workload >)
+    expect(getByText(testTitle)).not.toBeNull();
+    expect(mockAsyncFn.mock.calls.length).toBe(1);
+  });
 
-        expect(getByText(testTitle)).not.toBeNull();
-        expect(mockAsyncFn.mock.calls.length).toBe(1);
-    })
+  // this may cause an `act` warning in testing (https://github.com/testing-library/react-testing-library/issues/281)
+  // for now, just leave it alone
+  it("should update the whole page when refresh button is called", async () => {
+    const mockAsyncFn = jest.fn(async () => Promise.resolve([]));
 
-    it("should update the whole page when refresh button is called", () => {
-        const mockAsyncFn = jest.fn(async () => {
-            return Promise.resolve([])
-        })
+    const { findByText, getByRole, findByPlaceholderText } = render(
+      <Workload title={testTitle} table={{ fetchData: mockAsyncFn }}></Workload>
+    );
 
-        const { getByText, getByRole, getByPlaceholderText } = render(<Workload title={testTitle} table={{ fetchData: mockAsyncFn }} ></ Workload >)
-        userEvent.click(getByRole("button", { name: /refresh/i }));
+    userEvent.type(
+      await findByPlaceholderText("search title"),
+      "search string"
+    );
+    userEvent.click(getByRole("button", { name: /refresh/i }));
 
-        expect(getByText(testTitle)).not.toBeNull();
-        expect(getByPlaceholderText("search title").value).toBe("");
-        expect(mockAsyncFn.mock.calls.length).toBe(2);
-    })
+    await waitFor(() => expect(mockAsyncFn).toHaveBeenCalledTimes(2));
 
-})
+    expect(await findByText("No Data")).not.toBeNull(); // no data returned from faked request API
+    expect(await findByText(testTitle)).not.toBeNull(); // UI has been displayed properly
+    expect((await findByPlaceholderText("search title")).value).toBe(""); // search string has been updated
+  });
+});
